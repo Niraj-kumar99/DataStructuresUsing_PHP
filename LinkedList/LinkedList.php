f575190e6c77db9f8b28d90894bb4a84aead76b1
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-09-24 22:22:47 +0530

    Refactored: Added two more nodes and while loop in printList()

--- LinkedList/LinkedList.php
+++ LinkedList/LinkedList.php
@@ -13,8 +13,10 @@ class LinkedList {
         $temp = new Node();
         $temp = $this->head;
         if($temp != null) {
-            echo $temp->data." ";
-            $temp = $temp->next;
+            while($temp != null) {
+                echo $temp->data." ";
+                $temp = $temp->next;
+              }
         }
         else {
             echo "\nThe list is empty.";
@@ -31,6 +33,20 @@ $first->data = 10;
 $first->next = null;
 //Linking with head node.
 $MyLinkedList->head = $first;
+
+//Adding second node.
+$second = new Node();
+$second->data = 20;
+$second->next = null;
+//linking with first node
+$first->next = $second;
+
+//Adding third node.
+$third = new Node();
+$third->data = 30;
+$third->next = null;
+//linking with third node
+$second->next = $third;
 
 //Displaying List
 $MyLinkedList->printList();
